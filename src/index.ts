b806e96fc0735706658e5937ae83840b3b788dd3
flonix8 <flonix8@users.noreply.github.com>
2021-11-30 22:50:42 +0000

    Deploying to gh-pages from master @ e2f85355624998e478d8de4501dfeb43fc76f0b1 🚀

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 bootstrapExtra().catch(e => console.error(e));
 
 let currentPopup: any = undefined;
+let miroMessage : any = undefined;
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
 
@@ -14,6 +15,17 @@ WA.room.onEnterZone('clock', () => {
 })
 
 WA.room.onLeaveZone('clock', closePopUp)
+
+console.log("Hello World")
+
+WA.onEnterZone('miro-board', () => {
+    miroMessage = WA.ui.displayActionMessage({
+        message: "LEERTASTE drücken um Miro-Board in neuem Tab zu öffnen",
+        callback: () => WA.nav.openTab("https://miro.com/welcomeonboard/T3MwQ1RJS2NOTnlsSEdXSEJWbjFnRUNXSUtMQjJxOU5jaktneG03U01EY011VjNRRHJSeXVrY2JLc2ZnN0FTVXwzMDc0NDU3MzUyNTA4NTU1MjUz?invite_link_id=726956168519")
+    })
+})
+WA.onLeaveZone('miro-board', () => miroMessage.remove())
+
 
 function closePopUp(){
     if (currentPopup !== undefined) {
